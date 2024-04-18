61f576302d644f9ea718993e2208a14cb6026d09
Iannn <pristianb007@gmail.com>
2024-04-18 20:20:38 +0700

    OAuth & Middleware

--- app/auth-server-action/components/OAuthForm.tsx
+++ app/auth-server-action/components/OAuthForm.tsx
@@ -1,6 +1,25 @@
-import { Button } from "@/components/ui/button";
-import React from "react";
+import { Button } from '@/components/ui/button';
+import { createBrowserClient } from '@supabase/ssr';
+import React from 'react';
 
 export default function OAuthForm() {
-	return <Button className="w-full">Login With Github</Button>;
+  const supabase = createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+
+  const loginWithGithub = () => {
+    supabase.auth.signInWithOAuth({
+      provider: 'github',
+      options: {
+        redirectTo: `${location.origin}/auth-server-action/callback`,
+      },
+    });
+  };
+
+  return (
+    <Button className='w-full' onClick={loginWithGithub}>
+      Login With Github
+    </Button>
+  );
 }
